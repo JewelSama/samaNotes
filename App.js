6579cd9317e4424269e60e493db78a6651bdc626
Jewel Osafile <osafilejewel@gmail.com>
2022-10-04 10:50:41 +0100

    category and note screen

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Register from './screens/Register';
 import Login from './screens/Login';
+import Category from './screens/Category';
+import NoteScreen from './screens/NoteScreen';
 
 
 export default function App() {
@@ -16,6 +18,8 @@ export default function App() {
       }}>
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Category" component={Category} />
+        <Stack.Screen name="Note" component={NoteScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
