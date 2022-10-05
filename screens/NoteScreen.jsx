0d2cc4669968848a0ee4af23d563430c23886516
Jewel Osafile <osafilejewel@gmail.com>
2022-10-05 22:47:37 +0100

    POST new notes to backend && context

--- screens/NoteScreen.jsx
+++ screens/NoteScreen.jsx
@@ -1,15 +1,62 @@
-import { View, SafeAreaView, Text, TextInput, TouchableOpacity, ScrollView } from 'react-native'
+import { View, SafeAreaView, Text, TextInput, TouchableOpacity, ScrollView, Alert, Vibration, ActivityIndicator } from 'react-native'
 import React, {useState} from 'react'
 import GlobalStyles from '../GlobalStyles'
 import { Entypo } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { postNotesAPI } from '../endpoints';
+import { useContext } from 'react';
+import { GlobalContext } from '../context';
 
-const NoteScreen = () => {
-  const navigation = useNavigation()
-  const [category, setCategory] = useState('')
-  const [note, setNote] = useState('')
+const NoteScreen = ({navigation, route}) => {
+  // const navigation = useNavigation()
+  const {categoryT, data} = route.params
+  const [category, setCategory] = useState(categoryT)
+  const [note, setNote] = useState(data)
+  const [loading, setLoading] = useState(false)
+  
+  const {token, setNotes, notes} = useContext(GlobalContext)
+
+
+
+  const saveNoteHandler = () => {
+    if(note.length < 1){
+      return alert('Note cannot be empty')
+    }
+    if(!category){
+      return alert('category cannot be empty')
+    }
+    const formData = {
+      category: category,
+      note: note,
+    }
+    setLoading(true)
+    // console.log(typeof note);
+    fetch(postNotesAPI, {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }),
+      body: JSON.stringify(formData)
+    })
+    .then(res => res.json())
+    .then(resp => {
+      setLoading(false)
+      setNotes([resp, ...notes])
+      console.log(notes);
+      navigation.navigate('Category', {notes:notes})
+      Vibration.vibrate(1000)
+    })
+    .catch(err => {
+      setLoading(false)
+      Alert.alert('Error', err.message)
+      Vibration.vibrate(1000)
+      console.log(err.message);
+    })
+  }
+
 
   return (
     <>
@@ -17,13 +64,18 @@ const NoteScreen = () => {
           <TouchableOpacity onPress={() => navigation.goBack()}>
             <AntDesign name="left" size={24} color="rgb(107, 114, 128)" />
           </TouchableOpacity>
+            {note === data ? 
           <TouchableOpacity>
-            {!note ? 
-            <Entypo name="dots-three-vertical" size={20} color="rgb(107, 114, 128)" /> : (
-            <Feather name="check" size={26} color="rgb(107, 114, 128)" /> )
-            }
+            <Entypo name="dots-three-vertical" size={20} color="rgb(107, 114, 128)" /> 
           </TouchableOpacity>
+          : (
+              <TouchableOpacity onPress={saveNoteHandler}>
+                <Feather name="check" size={26} color="rgb(107, 114, 128)" /> 
+              </TouchableOpacity>
+            )
+            }
       </SafeAreaView>
+      {loading && <ActivityIndicator />}
       <Text className="text-center text-sm mt-2 font-semibold text-gray-400">May 27, 2021 at 2:57</Text>
       <TextInput 
         className="text-xl px-6 mt-4 font-bold"
